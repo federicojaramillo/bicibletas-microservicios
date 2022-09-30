4c4fa858c6b6fc73c07760dc02a814e9aa7d2d84
Federico <fjaramillof@gmail.com>
2022-09-30 17:21:24 -0500

    Front end con google signin

---
+++ frontend/src/App.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import Button from '@mui/material/Button';
+import AppBar from '@mui/material/AppBar';
+import Container from '@mui/material/Container';
+import Toolbar from '@mui/material/Toolbar';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import PedalBikeIcon from '@mui/icons-material/PedalBike';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+import CustomMap from "./components/CustomMap";
+import GoogleLoginButton from "./components/GoogleLoginButton";
+
+const theme = createTheme();
+
+export default function App() {
+  
+  return (
+    <ThemeProvider theme={theme}>
+    <AppBar position="relative">
+    <Toolbar>
+    <PedalBikeIcon sx={{ mr: 2 }} />
+          <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
+            Red de Bicicletas
+          </Typography>
+          <GoogleLoginButton/>
+          <Button variant="contained" sx={{ ml: 2 }}>Hello World</Button>
+        </Toolbar>
+      </AppBar>
+      <Container component="main" maxWidth="md" sx={{ mb: 4 }}>
+        <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
+        <Typography component="h1" variant="h4" align="center">
+            Mapa de las bicicletas
+          </Typography>
+          <div>
+          <CustomMap />
+        </div>
+        </Paper>
+        </Container>
+  </ThemeProvider>
+  );
+}
+
---
+++ frontend/src/components/GoogleLoginButton.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import { gapi } from 'gapi-script';
+
+const GoogleLoginButton = () => {
+    const [ profile, setProfile ] = useState([]);
+    const clientId = '311903986104-hr7uc61b1mmq2fc6c4ie8iutj92mi3ui.apps.googleusercontent.com';
+    useEffect(() => {
+        const initClient = () => {
+            gapi.client.init({
+                clientId: clientId,
+                scope: ''
+            });
+        };
+        gapi.load('client:auth2', initClient);
+        setProfile(JSON.parse(localStorage.getItem('userInfo')));
+    });
+
+    const onSuccess = (res) => {
+        setProfile(res.profileObj);
+        localStorage.setItem('userInfo', JSON.stringify(res.profileObj));
+    };
+
+    const onFailure = (err) => {
+        console.log('failed', err);
+    };
+
+    const logOut = () => {
+        setProfile(null);
+        localStorage.clear();
+    };
+
+    return (
+        <div>
+            {profile ? (
+                <div>
+                    {profile.name + ' '}
+                    <GoogleLogout clientId={clientId} buttonText="Log out" onLogoutSuccess={logOut} />
+                </div>
+            ) : (
+                <GoogleLogin
+                    clientId={clientId}
+                    buttonText="Sign in with Google"
+                    onSuccess={onSuccess}
+                    onFailure={onFailure}
+                    cookiePolicy={'single_host_origin'}
+                    isSignedIn={true}
+                />
+            )}
+        </div>
+    );
+}
+
+export default GoogleLoginButton;
